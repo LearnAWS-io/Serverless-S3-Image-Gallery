4ec182af394d8e03eb47238fcd1a293f8ddbbf7a
Shivam <singhshivamkr@gmail.com>
2023-06-24 16:56:49 +0530

    :bento: show images with signed url

--- lambda/hello-aws.ts
+++ lambda/hello-aws.ts
@@ -1,4 +1,9 @@
-import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
+import {
+  GetObjectCommand,
+  ListObjectsV2Command,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 
 const { BUCKET_NAME } = process.env;
@@ -16,15 +21,24 @@ export const handler: APIGatewayProxyHandlerV2 = async () => {
   });
   const s3ObjectListRes = await s3Client.send(s3ListObjectsCmd);
 
-  const objectLi = s3ObjectListRes?.Contents?.map(
-    (item) => `<li>${item.Key}</li>`
+  const presignReqListPromise = s3ObjectListRes?.Contents?.map((item) => {
+    const getObjCmd = new GetObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: item.Key,
+    });
+    return getSignedUrl(s3Client, getObjCmd, { expiresIn: 300 });
+  });
+
+  if (!presignReqListPromise) {
+    throw Error("Failed to create presigned requests");
+  }
+
+  const presignReqRes = await Promise.all(presignReqListPromise);
+
+  const imgList = presignReqRes.map(
+    (url) => `<img src=${url} width="120" height="120"/>`
   );
-  const htmlStr = objectLi
-    ? `<ul>
-  ${objectLi.join("\n")}
-</ul>
-`
-    : "<h1>No objects found</h1>";
+  const htmlStr = imgList ? imgList.join("\n") : "<h1>No images found</h1>";
 
   return {
     body: htmlStr,
