8a6abfb57035eab88bad9e15b6fc6baab6eef34e
Shivam <singhshivamkr@gmail.com>
2023-07-26 13:59:15 +0530

    :sparkles: add handlebars for rendering .html

--- lambda/list-images-fn.ts
+++ lambda/list-images-fn.ts
@@ -5,6 +5,8 @@ import {
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { readFile } from "fs/promises";
+import Handlebars from "handlebars";
 
 const { BUCKET_NAME } = process.env;
 
@@ -14,40 +16,65 @@ if (!BUCKET_NAME) {
   throw Error("Missing bucket name in ENV");
 }
 
-export const handler: APIGatewayProxyHandlerV2 = async () => {
-  const s3ListObjectsCmd = new ListObjectsV2Command({
+const s3ListObjectsCmd = new ListObjectsV2Command({
+  Bucket: BUCKET_NAME,
+  // Prefix: "aws-service",
+});
+
+const s3ObjectListRes = await s3Client.send(s3ListObjectsCmd);
+
+const presignReqListPromise = s3ObjectListRes?.Contents?.map((item) => {
+  const getObjCmd = new GetObjectCommand({
     Bucket: BUCKET_NAME,
-    // Prefix: "aws-service",
+    Key: item.Key,
   });
-  const s3ObjectListRes = await s3Client.send(s3ListObjectsCmd);
+  return getSignedUrl(s3Client, getObjCmd, { expiresIn: 300 });
+});
 
-  const presignReqListPromise = s3ObjectListRes?.Contents?.map((item) => {
-    const getObjCmd = new GetObjectCommand({
-      Bucket: BUCKET_NAME,
-      Key: item.Key,
-    });
-    return getSignedUrl(s3Client, getObjCmd, { expiresIn: 300 });
-  });
+let image_list = null;
 
-  if (!presignReqListPromise) {
-    throw Error("Failed to create presigned requests");
-  }
-
+if (presignReqListPromise) {
+  // throw Error("Failed to create presigned requests");
   const presignReqRes = await Promise.all(presignReqListPromise);
 
-  const imgList = presignReqRes.map(
-    (url) => `<img src=${url} width="120" height="120"/>`
-  );
-  const htmlStr = imgList ? imgList.join("\n") : "<h1>No objects found</h1>";
+  image_list = presignReqRes.map((url) => {
+    const alt = url
+      .split("/")[4]
+      .split("?")[0]
+      .slice(0, -4)
+      .replaceAll("-", " ");
+    return {
+      url,
+      alt,
+    };
+  });
+}
 
+const htmlImgTemplate = await readFile("./image-list.html", {
+  encoding: "utf8",
+});
+
+const template = Handlebars.compile(htmlImgTemplate);
+
+let compiledHtmlStr = "Unable to render HTML";
+
+try {
+  compiledHtmlStr = template({
+    image_list,
+    image_count: s3ObjectListRes.KeyCount,
+  });
+} catch (err) {
+  if (err instanceof Error) {
+    compiledHtmlStr = JSON.stringify(err?.message);
+  }
+}
+
+export const handler: APIGatewayProxyHandlerV2 = async (e) => {
   return {
-    body: htmlStr,
+    body: compiledHtmlStr,
     statusCode: 200,
     headers: {
       "Content-Type": "text/html",
     },
   };
 };
-
-//@ts-ignore
-// handler();
